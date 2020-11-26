b2f397a2d95150ddeef97ba4d9e52e2221a71146
Abel Toth <abeltoth7@gmail.com>
2020-11-27 00:16:42 +0100

    Menu responsiveness added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './components/menu/menu.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)'}),
+        animate('500ms', style({ transform: 'translateY(0)'}))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(0)'}),
+        animate('500ms', style({ transform: 'translateY(-100%)'}))
+      ])
+    ]
+    )
+  ],
 })
 export class MenuComponent implements OnInit {
+
+  isMenuItemsVisible = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if (event.target.innerWidth > 768) {
+      this.isMenuItemsVisible = false;
+    }
   }
 
 }
